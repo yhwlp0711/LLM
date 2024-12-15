a151be62306a04fcba242d8aeb6234e6f8e57f6f
Kasumi <yhwlp0711@gmail.com>
2024-12-16 00:28:07 +0800

    2024.12.16 from Windows

--- Llama.py
+++ Llama.py
@@ -1,2 +1,19 @@
-from models.llama import LlamaModel, LlamaConfig
+from transformers.models.llama import LlamaModel, LlamaConfig
 import torch
+
+config = LlamaConfig(
+    vocab_size=32000,
+    hidden_size=512,
+    intermediate_size=2048,
+    num_hidden_layers=6,
+    num_attention_heads=8,
+    max_position_embeddings=512,
+    use_cache=False
+    )
+
+llama = LlamaModel(config)
+
+X = torch.randint(low=0, high=32000, size=(4, 100))  # (batch_size, seq_len)
+
+output = llama(X)
+print(output)  # torch.Size([4, 100, 512])
